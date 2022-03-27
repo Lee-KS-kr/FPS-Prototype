aa8130a0cec5f8831834ba20c8ce0fd8efb901d0
Kyungseo Lee <kyungseolee0815@gmail.com>
2022-03-27 20:26:03 +0900

    player HUD

--- Assets/Scripts/WeaponAssaultRiffle.cs
+++ Assets/Scripts/WeaponAssaultRiffle.cs
@@ -3,8 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class AmmoEvent : UnityEngine.Events.UnityEvent<int, int> {  }
+
 public class WeaponAssaultRiffle : MonoBehaviour
 {
+    [HideInInspector] public AmmoEvent onAmmoEvent = new AmmoEvent();
+    
     [Header("Fire Effects")] [SerializeField]
     private GameObject muzzleFlashEffect; // 총구 이펙트 on/off
 
@@ -23,12 +28,18 @@ public class WeaponAssaultRiffle : MonoBehaviour
     private AudioSource audioSource; // 사운드 재생 컴포넌트
     private PlayerAnimatorController animator; // 애니메이션 재생 제어
     private CasingMemoryPool casingMemoryPool; // 탄피 생성 후 활성/비활성 관리
+    
+    // 외부에서 필요한 정보를 열람하기 위해 정의한 Get Property
+    public WeaponName WeaponName => weaponSetting.weaponName;
 
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
         animator = GetComponentInParent<PlayerAnimatorController>();
         casingMemoryPool = GetComponent<CasingMemoryPool>();
+        
+        // 처음 탄 수는 최대로 설정
+        weaponSetting.currentAmmo = weaponSetting.maxAmmo;
     }
 
     private void OnEnable()
@@ -37,6 +48,8 @@ public class WeaponAssaultRiffle : MonoBehaviour
         PlaySound(audioClipTakeOutWeapon);
         // 총구 이펙트 오브젝트 비활성화
         muzzleFlashEffect.SetActive(false);
+        // 무기가 활성화될 때 해당 무기의 탄 수 정보를 갱신한다
+        onAmmoEvent.Invoke(weaponSetting.currentAmmo, weaponSetting.maxAmmo);
     }
 
     private void PlaySound(AudioClip clip)
@@ -95,6 +108,15 @@ public class WeaponAssaultRiffle : MonoBehaviour
             
             // 공격주기가 되어가 공격할 수 있도록 하기 위해 현재 시간 저장
             lastAttackTime = Time.time;
+            
+            // 탄 수가 없으면 공격 불가능
+            if (weaponSetting.currentAmmo <= 0)
+            {
+                return;
+            }
+            // 공격시 currenAmmo 1 감소
+            weaponSetting.currentAmmo--;
+            onAmmoEvent.Invoke(weaponSetting.currentAmmo, weaponSetting.maxAmmo);
             
             // 무기 애니메이션 재생
             animator.Play("Fire",-1,0);
